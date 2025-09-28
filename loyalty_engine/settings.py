761977ce279109ccacd5339a077e7e1baab19478
Jagan <166379843+JagadeshwaranK@users.noreply.github.com>
2025-09-28 19:55:37 +0530

    Add import os and use environment variable for SECRET_KEY

--- loyalty_engine/settings.py
+++ loyalty_engine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oPvzQTwgx3lN5uMyvLJG4xiB81XPqb2aEPbggAUtJbQQY7QcjuY-wiDusJ9xybbX73MQ'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'oPvzQTwgx3lN5uMyvLJG4xiB81XPqb2aEPbggAUtJbQQY7QcjuY-wiDusJ9xybbX73MQ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
