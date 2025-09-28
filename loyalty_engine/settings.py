550781b2cc97c21d12ddda911aade8dff89592fb
Jagan <166379843+JagadeshwaranK@users.noreply.github.com>
2025-09-28 19:57:33 +0530

    Update DEBUG and ALLOWED_HOSTS to use environment variables

--- loyalty_engine/settings.py
+++ loyalty_engine/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'oPvzQTwgx3lN5uMyvLJG4xiB81XPqb2aEPbggAUtJbQQY7QcjuY-wiDusJ9xybbX73MQ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',') if os.environ.get('ALLOWED_HOSTS') else []
 
 
 # Application definition
